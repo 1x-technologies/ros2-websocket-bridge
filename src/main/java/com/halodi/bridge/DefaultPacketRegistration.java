b49f3eae836436560690c2769a5aff37bedf818f
Jesper Smith <mail@jespersmith.nl>
2019-04-15 10:36:14 +0200

    Added vector3 to packet registration

--- src/main/java/com/halodi/bridge/DefaultPacketRegistration.java
+++ src/main/java/com/halodi/bridge/DefaultPacketRegistration.java
@@ -2,10 +2,10 @@ package com.halodi.bridge;
 
 import java.util.HashMap;
 
-import diagnostic_msgs.msg.dds.DiagnosticArray;
 import diagnostic_msgs.msg.dds.DiagnosticArrayPubSubType;
 import geometry_msgs.msg.dds.TwistPubSubType;
 import geometry_msgs.msg.dds.TwistStampedPubSubType;
+import geometry_msgs.msg.dds.Vector3PubSubType;
 import sensor_msgs.msg.dds.JointStatePubSubType;
 import std_msgs.msg.dds.Float64PubSubType;
 import trajectory_msgs.msg.dds.JointTrajectoryPubSubType;
@@ -23,6 +23,7 @@ public class DefaultPacketRegistration implements PacketRegistrationInterface
       topicDataTypes.put(TwistStampedPubSubType.name, new TwistStampedPubSubType());
       topicDataTypes.put(Float64PubSubType.name, new Float64PubSubType());
       topicDataTypes.put(DiagnosticArrayPubSubType.name, new DiagnosticArrayPubSubType());
+      topicDataTypes.put(Vector3PubSubType.name, new Vector3PubSubType());
    }
    
    @Override
